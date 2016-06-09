3458cef7529bfeb44147a8e846972e89eec56a3e
evanacooper <evan@brickworksoftware.com>
2016-06-08 20:34:41 -0700

    Update head.js

--- nike/head.js
+++ nike/head.js
@@ -1,21 +1,52 @@
 <script> var nsgConfig = {HOST: 'https://www.nike.com', PLACEMENT: 'prepend'} </script>
-<script type="text/javascript" src="http://www.nike.com/styleguide/init/nsg.js"></script>
-<link rel="stylesheet" type="text/css" media="screen" href="http://store.nike.com/us/en_us/style.css?app=nikestore&version=latest&assets=nike,nike.gadget.OneNikeNav,nike.gadget.OneNikeFooter&assetType=STYLE" />
-<script type="text/javascript" src="http://store.nike.com/us/en_us/NikeScriptManager.js?app=nikestore&assets=nike,nike.fonts.glyphPolyfill,nike.gadget.OneNikeNav,nike.gadget.OneNikeFooter,yepnope.injectCss&assetType=SCRIPT"></script>
 
 <script>
-  if ($(window).width() < 768) {
-    console.log("Mobile");
-    $('head').append('<script type="text/javascript" src="http://m.nike.com/us/en_us/NikeScriptManager.js?app=nikestore&version=latest&assets=nike,nike.gadget.mobile.OneNikeFooter,nike.gadget.mobile.OneNikeNav&assetType=SCRIPT"/>');
-  }
-  else {
-    console.log("Desktop");
-    $('head').append('<script type="text/javascript" src="http://store.nike.com/us/en_us/NikeScriptManager.js?app=nikestore&assets=nike,nike.fonts.glyphPolyfill,nike.gadget.OneNikeNav,nike.gadget.OneNikeFooter,yepnope.injectCss&assetType=SCRIPT"/>');
-  }
-</script>
+	var bwNikeLinks = {
+		uat: {
+			styles: {
+				base: 'http://ecn100-store.nikedev.com/us/en_us/style.css?app=nikestore&version=latest&assets=nike,nike.gadget.OneNikeNav,nike.gadget.OneNikeFooter&assetType=STYLE',
+				mobile: 'http://ecn100-m.nikedev.com/us/en_us/style.css?app=nikestore&version=latest&assets=nike.gadget.mobile.OneNikeFooter,nike.gadget.mobile.OneNikeNav,skin.onenike_mobile&assetType=STYLE'
+			},
+			scripts: {
+				nsgjs: 'http://ecn100-www.nikedev.com/styleguide/init/nsg.js',
+				nikeScriptManager: {
+					desktop: 'http://ecn100-store.nikedev.com/us/en_us/NikeScriptManager.js?app=nikestore&assets=nike,nike.fonts.glyphPolyfill,nike.gadget.OneNikeNav,nike.gadget.OneNikeFooter,yepnope.injectCss&assetType=SCRIPT',
+					mobile: 'http://ecn100-m.nikedev.com/us/en_us/NikeScriptManager.js?app=nikestore&version=latest&assets=nike,nike.gadget.mobile.OneNikeFooter,nike.gadget.mobile.OneNikeNav&assetType=SCRIPT'
+				}
+			}
+		},
+		prod: {
+			styles: {
+				base: 'http://store.nike.com/us/en_us/style.css?app=nikestore&version=latest&assets=nike,nike.gadget.OneNikeNav,nike.gadget.OneNikeFooter&assetType=STYLE',
+				mobile: 'http://m.nike.com/us/en_us/style.css?app=nikestore&version=latest&assets=nike.gadget.mobile.OneNikeFooter,nike.gadget.mobile.OneNikeNav,skin.onenike_mobile&assetType=STYLE'
+			},
+			scripts: {
+				nsgjs: 'http://www.nike.com/styleguide/init/nsg.js',
+				nikeScriptManager: {
+					desktop: 'http://store.nike.com/us/en_us/NikeScriptManager.js?app=nikestore&assets=nike,nike.fonts.glyphPolyfill,nike.gadget.OneNikeNav,nike.gadget.OneNikeFooter,yepnope.injectCss&assetType=SCRIPT',
+					mobile: 'http://m.nike.com/us/en_us/NikeScriptManager.js?app=nikestore&version=latest&assets=nike,nike.gadget.mobile.OneNikeFooter,nike.gadget.mobile.OneNikeNav&assetType=SCRIPT'
+				}
+			}
+		}
+	}
+	
+	if (/ecn100/.test(window.location.href)) {
+		var bwNikeLinkEnv = 'uat'
+	} else {
+		var bwNikeLinkEnv = 'prod'
+	}
 
-<link rel="stylesheet" type="text/css" media="screen" href="http://m.nike.com/us/en_us/style.css?app=nikestore&version=latest&assets=nike.gadget.mobile.OneNikeFooter,nike.gadget.mobile.OneNikeNav,skin.onenike_mobile&assetType=STYLE"/>
-<link rel="stylesheet" type="text/css" media="screen" href="http://store.nike.com/us/en_us/style.css?app=nikestore&version=latest&assets=nike,nike.gadget.OneNikeNav,nike.gadget.OneNikeFooter&assetType=STYLE"/>
+	if (window.innerWidth < 768) {
+		var bwNikeLinkDevice = 'mobile'
+	} else {
+		var bwNikeLinkDevice = 'desktop'
+	}
+
+	document.write('<link rel="stylesheet" type="text/css" media="screen" href=' + bwNikeLinks[bwNikeLinkEnv].styles.base + '>')
+	document.write('<link rel="stylesheet" type="text/css" media="screen" href=' + bwNikeLinks[bwNikeLinkEnv].styles.mobile + '>')
+	document.write('<script type="text/javascript" src=' + bwNikeLinks[bwNikeLinkEnv].scripts.nsgjs + '><\/script>')
+	document.write('<script type="text/javascript" src=' + bwNikeLinks[bwNikeLinkEnv].scripts.nikeScriptManager[bwNikeLinkDevice] + '><\/script>')
+</script>
 
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.3.0.min.js"></script>

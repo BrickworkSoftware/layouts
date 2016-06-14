42503bcd18a45132d09fe468860ec16a00fb1e14
evanacooper <evan@brickworksoftware.com>
2016-06-14 16:14:14 -0400

    Update head_content.js

--- slt/head_content.js
+++ slt/head_content.js
@@ -4,13 +4,137 @@
 
 <link href="//www.surlatable.com/styles/global/nav.css" rel="stylesheet" type="text/css" />
 <link href="//www.surlatable.com/styles/global/styles.css" rel="stylesheet" type="text/css" />
+ <style>
 
-<style>
-  div#wrapper2 {
-    width: 100%;
-    max-width: 990px;
-    margin: 12px auto 0 auto;
-    text-align: left;
-    background: white;
-  } 
+/*Brick Work Styles*/
+@media (max-width: 967px){
+    .navbar-custom{
+        margin-top:0px !important;
+    }
+    #wrapper{
+        margin-top:0px !important;
+    }
+    .mobileLogo{
+        background: url("//assets.moovweb.net/6f88e45f-4cd5-46e0-a546-b589ab44ac5e/3f1db3ea-224a-4b05-a0b2-bb8405052cd7/v111/images/icons.png") no-repeat 0px -255px;
+        background-size: 295px 584px;
+        width: 138px;
+        height: 32px;
+        margin-top: 10px;
+    }
+    .navbar-toggle{
+        float:left;
+        margin-left:5px;
+    }
+    #searchMobile>div#topNavSearchMobile {
+        right: inherit;
+        width: 100%;
+        top:-20px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        padding-left: 25px;
+        padding-right: 35px;
+    }
+    #searchMobile .form-control {
+        display: inline-block;
+        width: 100%;
+        vertical-align: middle;
+    }
+    #searchMobile>#topNavSearchMobile>input {
+        height: inherit;
+        margin: auto;
+        width: 100%;
+    }
+    #searchMobile>#topNavSearchMobile .btn-default{
+       background: #444;
+       border:#444;
+       color:#fff;
+        font-size: .9em;
+        padding: 10px 15px;
+    }
+
+    .shoppingCartBtn{
+        float: right;
+        padding: 10px 20px;
+        position: absolute;
+        right: 0;
+        top:0;
+        cursor: pointer;
+        border-left: #56902B solid 1px;
+        font-size: 1.4em !important;
+    }
+}
+
+
+div#footer #epilogue{
+        width:100%;
+    }
+    div#footer #epilogue .menu{
+        width:33.333333%;
+    }
+    div#footer #social ul li{
+        float:inherit;
+    }
+    div#footer #email, div#footer #social, div#footer #copyright{
+        width:100%;
+        text-align:center;
+    }
+
+
+
+ @font-face {
+    font-family: 'Glyphicons Halflings';
+    src: url("fonts/bootstrap/glyphicons-halflings-regular.eot");
+    src: url("fonts/bootstrap/glyphicons-halflings-regular.eot?#iefix") format("embedded-opentype"),url("fonts/bootstrap/glyphicons-halflings-regular.woff2") format("woff2"),url("fonts/bootstrap/glyphicons-halflings-regular.woff") format("woff"),url("fonts/bootstrap/glyphicons-halflings-regular.ttf") format("truetype"),url("fonts/bootstrap/glyphicons-halflings-regular.svg#glyphicons_halflingsregular") format("svg")
+}
+ @font-face {
+    font-family: 'MrEavesSansRegular';
+    src: url("fonts/MrEaves/MrEavesSansRegularAlternates/mreavessanaltr-071211002EmigreWebOnly.eot");
+    src: url("fonts/MrEaves/MrEavesSansRegularAlternates/mreavessanaltr-071211002EmigreWebOnly.eot?#iefix") format("embedded-opentype"),url("fonts/MrEaves/MrEavesSansRegularAlternates/mreavessanaltr-071211002EmigreWebOnly.woff") format("woff")
+}
+
+@font-face {
+    font-family: 'MrEavesModernBold';
+    src: url("fonts/MrEaves/MrEavesModernBoldAlternates/mreavesmodaltb-071211001EmigreWebOnly.eot");
+    src: url("fonts/MrEaves/MrEavesModernBoldAlternates/mreavesmodaltb-071211001EmigreWebOnly.eot?#iefix") format("embedded-opentype"),url("fonts/MrEaves/MrEavesModernBoldAlternates/mreavesmodaltb-071211001EmigreWebOnly.woff") format("woff")
+}
+
+@font-face {
+    font-family: 'MrEavesModernRegular';
+    src: url("fonts/MrEaves/MrEavesModernRegularAlternates/mreavesmodaltr-071211005EmigreWebOnly.eot");
+    src: url("fonts/MrEaves/MrEavesModernRegularAlternates/mreavesmodaltr-071211005EmigreWebOnly.eot?#iefix") format("embedded-opentype"),url("fonts/MrEaves/MrEavesModernRegularAlternates/mreavesmodaltr-071211005EmigreWebOnly.woff") format("woff")
+}
+
+@font-face {
+    font-family: 'MrEavesSansBold';
+    src: url("fonts/MrEaves/MrEavesSansBoldAlternates/mreavessanaltb-071211002EmigreWebOnly.eot");
+    src: url("fonts/MrEaves/MrEavesSansBoldAlternates/mreavessanaltb-071211002EmigreWebOnly.eot?#iefix") format("embedded-opentype"),url("fonts/MrEaves/MrEavesSansBoldAlternates/mreavessanaltb-071211002EmigreWebOnly.woff") format("woff")
+}
+
+@font-face {
+    font-family: 'MrEavesSansRegular';
+    src: url("fonts/MrEaves/MrEavesSansRegularAlternates/mreavessanaltr-071211002EmigreWebOnly.eot");
+    src: url("fonts/MrEaves/MrEavesSansRegularAlternates/mreavessanaltr-071211002EmigreWebOnly.eot?#iefix") format("embedded-opentype"),url("fonts/MrEaves/MrEavesSansRegularAlternates/mreavessanaltr-071211002EmigreWebOnly.woff") format("woff")
+}
+
+@font-face {
+    font-family: 'MrEavesSansBoldItalic';
+    src: url("fonts/MrEaves/MrEavesSansBoldItalicAlternates/mreavessanaltbi-071211002EmigreWebOnly.eot");
+    src: url("fonts/MrEaves/MrEavesSansBoldItalicAlternates/mreavessanaltbi-071211002EmigreWebOnly.eot?#iefix") format("embedded-opentype"),url("fonts/MrEaves/MrEavesSansBoldItalicAlternates/mreavessanaltbi-071211002EmigreWebOnly.woff") format("woff")
+}
+
+@font-face {
+    font-family: 'MrEavesSansRegularItalic';
+    src: url("fonts/MrEaves/MrEavesSansRegularItalicAlternates/mreavessanaltri-071211002EmigreWebOnly.eot");
+    src: url("fonts/MrEaves/MrEavesSansRegularItalicAlternates/mreavessanaltri-071211002EmigreWebOnly.eot?#iefix") format("embedded-opentype"),url("fonts/MrEaves/MrEavesSansRegularItalicAlternates/mreavessanaltri-071211002EmigreWebOnly.woff") format("woff")
+}
+
 </style>
+
+
+<script type="text/javascript">
+  
+  $(document).ready(function(){
+    $("#SLTHeader").load("header.html");
+    $("#SLTFooter").load("footer.html");
+  });
+</script>

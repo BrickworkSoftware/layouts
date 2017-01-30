35542a6250c6e797a33d5aa31b8e02988fe6514a
evanacooper <evan@brickworksoftware.com>
2017-01-30 10:37:10 -0500

    Create head.js

---
+++ saks_off_5th/uat/head.js
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="http://content.hbc.com/etc/ui/hbc/clientlibs/framework_commons_off5th.d992c2efa6ba3a65f115a0a55c3f9ecf.css" type="text/css">
+
+<script type="text/javascript" src="http://content.hbc.com/etc/ui/hbc/clientlibs/modernizr.914c2f1712542db5abb2d6cedac0c521.js"></script>
+
+<script type="text/javascript" src="http://content.hbc.com/etc/ui/hbc/clientlibs/mbox.bf464603da03f42f30e489e387620bd1.js"></script>
+
+<script type="text/javascript" src="http://content.hbc.com/etc/ui/hbc/clientlibs/framework_polyfill.d8d3c5c662a3de4c05a12a534918b73e.js"></script>
+
+
+<link rel="stylesheet" href="http://hbc-prod6.adobecqms.net/styles/overrides.css" type="text/css">

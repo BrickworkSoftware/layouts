71e85bad614c93b52280cbaaa6c9e77242b585e7
evanacooper <evan@brickworksoftware.com>
2017-02-22 13:48:42 -0500

    Update head.js

--- saks_off_5th/production/head.js
+++ saks_off_5th/production/head.js
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="//content.hbc.com/etc/ui/hbc/clientlibs/framework_commons_off5th.9ef555e6c0ba48ea0ccf053433c5c376.css" type="text/css">
+<link rel="stylesheet" href="//content.hbc.com/etc/ui/hbc/clientlibs/framework_commons_off5th.f56cd31717f97fe0644e7ad466325d55.css" type="text/css">
 
 <script type="text/javascript" src="//content.hbc.com/etc/ui/hbc/clientlibs/modernizr.914c2f1712542db5abb2d6cedac0c521.js"></script>
 
-<script type="text/javascript" src="//content.hbc.com/etc/ui/hbc/clientlibs/framework_polyfill.a0dbe6b484d72e145e4efa65b356f649.js"></script>
+<script type="text/javascript" src="//content.hbc.com/etc/ui/hbc/clientlibs/framework_polyfill.8b9dad12a3e1c5cab15d3e465c692a7a.js"></script>

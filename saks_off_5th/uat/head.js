e1dfa8b6597e734e9d134f46c3434078a3a25780
evanacooper <evan@brickworksoftware.com>
2017-02-23 12:31:45 -0500

    Update head.js

--- saks_off_5th/uat/head.js
+++ saks_off_5th/uat/head.js
@@ -1,10 +1,5 @@
-<link rel="stylesheet" href="http://content.hbc.com/etc/ui/hbc/clientlibs/framework_commons_off5th.d992c2efa6ba3a65f115a0a55c3f9ecf.css" type="text/css">
+<link rel="stylesheet" href="//hbc-stage6.adobecqms.net/etc/ui/hbc/clientlibs/framework_commons_off5th.f56cd31717f97fe0644e7ad466325d55.css" type="text/css">
 
-<script type="text/javascript" src="http://content.hbc.com/etc/ui/hbc/clientlibs/modernizr.914c2f1712542db5abb2d6cedac0c521.js"></script>
+<script type="text/javascript" src="//hbc-stage6.adobecqms.net/etc/ui/hbc/clientlibs/modernizr.914c2f1712542db5abb2d6cedac0c521.js"></script>
 
-<script type="text/javascript" src="http://content.hbc.com/etc/ui/hbc/clientlibs/mbox.bf464603da03f42f30e489e387620bd1.js"></script>
-
-<script type="text/javascript" src="http://content.hbc.com/etc/ui/hbc/clientlibs/framework_polyfill.d8d3c5c662a3de4c05a12a534918b73e.js"></script>
-
-
-<link rel="stylesheet" href="http://hbc-prod6.adobecqms.net/styles/overrides.css" type="text/css">
+<script type="text/javascript" src="//hbc-stage6.adobecqms.net/etc/ui/hbc/clientlibs/framework_polyfill.8b9dad12a3e1c5cab15d3e465c692a7a.js"></script>

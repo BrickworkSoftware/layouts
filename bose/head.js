eacaef25e3897aee4fd4cea341762b92c46eab62
Kyle Salmon <kyleraysalmon@gmail.com>
2018-06-13 13:07:49 -0400

    Create head.js

---
+++ bose/head.js
@@ -0,0 +1,66 @@
+<link href="//static.bose.com/etc/designs/bose/consumer-products-2016/design.c14b5b47c8c4f3534ae38e7a090f8da1.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="//static.bose.com/etc/clientlibs/bose/common/components/buttonLink/clientlibs-b2c.733664df3319bde47ecbe5c94644b56d.css" type="text/css">
+<link rel="stylesheet" href="//static.bose.com/etc/clientlibs/bose/myaccount/components/accountWidget/clientlibs-b2c.794f610cbed9005bb1b5499272cb3972.css" type="text/css">
+<link rel="stylesheet" href="//static.bose.com/etc/clientlibs/bose/common/libraries/slick.73b016bda76f48d27944d3bd0ae3707a.css" type="text/css">
+<link rel="stylesheet" href="//static.bose.com/etc/clientlibs/bose/common/components/imageGallery/clientlibs-b2c.79970f88b2bdb8a75dcdbaec9e01d227.css" type="text/css">
+<link rel="stylesheet" href="//static.bose.com/etc/clientlibs/bose/ecommerce/components/productImageSlider/clientlibs-b2c.a2e167854f7723d0ef38df371131e7db.css" type="text/css">
+<link rel="stylesheet" href="//static.bose.com/etc/clientlibs/bose/ecommerce/components/paypalBanner/clientlibs-b2c.33f2562dd01402101fc203b8ba226819.css" type="text/css">
+<link rel="stylesheet" href="//static.bose.com/etc/clientlibs/bose/common/components/backToTopButton/clientlibs-b2c.90546b05e19c6a110fb20b6d4d2896f9.css" type="text/css">
+<link rel="stylesheet" href="//static.bose.com/etc.clientlibs/bose/forms/components/select/clientlibs.c74db72609c629ea7e98d43ff7ebdc2c.css" type="text/css">
+<link rel="stylesheet" href="//static.bose.com/etc.clientlibs/bose/ecommerce/components/ratingsAndReviews/clientlibs.650e67c6a209a5431ca0369126c98c0f.css" type="text/css">
+<script type="text/javascript" src="//static.bose.com/etc.clientlibs/clientlibs/granite/jquery.fb50358df4c2bd6aa6e1dd5b0d9b9d29.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc.clientlibs/clientlibs/granite/utils.b865d2b414814302a14b06054b028a9c.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/libraries/jquery.5c00be2127f14b8fbba61ca34916aeb2.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/libraries/jqueryMatchHeight.3e62ee988fb48b0bbe3ec1283a514f62.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/functions/bose-utils.ae2ff7a27d7d41e987c0044e285b9471.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/libraries/js-cookie.f196966111958ffed8e027d871da0489.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/libraries/bose-cookieStore.364c6bceb2a9d64ff1adb7b1916a1754.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/libraries/bose-datalayer.27f262e4eb6b8eadcaf53f4733fc57b9.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/libraries/fancyBox.24f2b7736a627f1ccea9296475d30240.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/libraries/gridle.e293361602e716ac7991abb106ab0dde.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/libraries/bose-gridle.14dbe6b3515a0fa5d2a28bd29fd10830.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/components/buttonLink/clientlibs-b2c.670fa425636fffeb5c01d458e072c20a.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/functions/bose-translation.9f4054872d1c3086e8889e6127f2258a.js"></script>
+<script type="text/javascript" src="//static.bose.com/etc/clientlibs/bose/common/javascript-head-all.e7ff211948fe6cae17b57d79268d88ea.js"></script>
+<link rel="canonical" href="https://www.bose.com/en_us/brickworkApp.html"/>
+<!--/* Alternate sites */ -->
+<script>
+    var categoryPathSegments = Bose.Utils.getCategoryPathSegments();
+
+    Bose.DataLayer.setPageVariable("path", "https:\/\/www.bose.com\/en_us\/brickworkApp.html");
+    Bose.DataLayer.setPageVariable("name", "brickworkApp");
+    Bose.DataLayer.setPageVariable("referrerUrl", document.referrer);
+    Bose.DataLayer.setPageVariable("type", "contentpage");
+    Bose.DataLayer.setPageVariable("country", "us");
+    Bose.DataLayer.setPageVariable("countryFullEnglish", "united_states");
+    Bose.DataLayer.setPageVariable("language", "en");
+    Bose.DataLayer.setPageVariable("languageFullEnglish", "english");
+    Bose.DataLayer.setPageVariable("locale", "en\u002DUS");
+    Bose.DataLayer.setPageVariable("RID", Bose.Utils.getParameterByName("ET_RID"));
+    Bose.DataLayer.setPageVariable("CID", Bose.Utils.getParameterByName("ET_CID"));
+    Bose.DataLayer.setPageVariable("formattedLocale", "en_us");
+    Bose.DataLayer.setPageVariable("dataCaptureEnabled", "true");
+    Bose.DataLayer.setPageVariable('primaryCategory', categoryPathSegments[0]);
+
+    for (var i = 1; i < categoryPathSegments.length; i++) {
+        // Skip first as it is the primary category.
+        Bose.DataLayer.setPageVariable('subCategory' + i, categoryPathSegments[i]);
+    }
+
+    Bose.DataLayer.setSettingsVariable('microServiceUrl', "https:\/\/cdn.prod02.nebula.run\/orion\/");
+    Bose.DataLayer.setSettingsVariable("geolocationEnabled", "true");
+    Bose.DataLayer.setSettingsVariable("ecommerceEnabled", "true");
+
+    Bose.DataLayer.readCookies();
+
+</script>
+<script type="text/javascript">var inHeadTS = (new Date()).getTime();</script>
+<script src="//assets.adobedtm.com/cef3916be2046b4528c40938d03aebfc252344b7/satelliteLib-cf93dcb9ef102784de98dd91b70b30eba13e6752.js"></script>
+<link rel="icon" type="image/vnd.microsoft.icon" href="//static.bose.com/etc/designs/bose/consumer-products-2016/favicon.ico"/>
+<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="//static.bose.com/etc/designs/bose/consumer-products-2016/favicon.ico"/>
+<title>brickworkApp</title>
+<meta property="og:title" content="brickworkApp">
+<meta property="og:image" content="https://static.bose.com/etc/designs/bose/base2016/design/images/bose_logo_white_on_black.jpg">
+<script>
+    $('body').addClass('-fullWidth')
+</script>
